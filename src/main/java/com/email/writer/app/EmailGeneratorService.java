8be0050790a77bc60563efd56199fbaf11440f40
Vedant <vedantdumbre0706@gmail.com>
2025-07-08 21:32:57 +0530

    Making Serive file

---
+++ src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,11 @@
+package com.email.writer.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+
+    public String generateEmailReply(EmailRequest emailRequest) {
+        //
+    }
+}
